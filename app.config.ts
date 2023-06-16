613002b5584fe2404e59d63cc47a95b219a8cfb3
fhoiland <64558753+fhoiland@users.noreply.github.com>
2023-06-16 19:18:33 +0200

    update title

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
+    title: 'Hoiland',
     description: 'The minimalist blog theme',
     image: {
       src: '/social-card-preview.png',
